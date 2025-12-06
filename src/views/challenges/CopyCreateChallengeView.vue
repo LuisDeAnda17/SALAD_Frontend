603f082f37ee8b30c7f2bb332d960813d99258d6
Alexis <172384082+alexissyi@users.noreply.github.com>
2025-12-05 22:45:41 -0500

    Add back a button

--- src/views/challenges/CopyCreateChallengeView.vue
+++ src/views/challenges/CopyCreateChallengeView.vue
@@ -121,6 +121,22 @@ async function resetForm() {
     <!-- CATEGORY SELECTION -->
     <div class="section">
       <h2>Select Category</h2>
+      <div class="button-row">
+        <button
+          class="select-btn"
+          :class="{ active: category === 'aerobic' }"
+          @click="
+            () => {
+              category = 'aerobic'
+              subcategory = null
+              submitted = false
+              failed = false
+            }
+          "
+        >
+          Aerobic
+        </button>
+      </div>
     </div>
   </div>
 </template>
